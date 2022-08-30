45d97a794fe450ea5160617c75fd9d1f0c15f370
masouto <matheusp.souto@yahoo.com.br>
2022-08-30 12:17:29 -0300

    Commit

---
+++ ex4.rb
@@ -0,0 +1,9 @@
+puts "Enter a noun"
+noun = gets.chomp
+puts "Enter a verb"
+verb = gets.chomp
+puts "Enter an adjective"
+adje = gets.chomp
+puts "Enter an adverb"
+adve = gets.chomp
+puts "The best way to #{verb} with #{adje} #{noun} is #{adve}." + " This's Funny"
