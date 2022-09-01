2e294edd2b0d9bcdba0d8788363febf38874c819
masouto <matheusp.souto@yahoo.com.br>
2022-09-01 11:52:30 -0300

    Commit

---
+++ ex3d.rb
@@ -0,0 +1,4 @@
+names = ["Steve Jobs", "Elon Musk", "Chaves"]
+quotes = ["Apple is mine", "Tesla is mine", "Isso,Isso,Isso"]
+number = rand(names.length())
+puts "#{names[number]} said," + "\"#{quotes[number]}\""
