1ae89ecb80916010df2904edf12e1ebcb298c5c0
masouto <matheusp.souto@yahoo.com.br>
2022-09-01 10:26:28 -0300

    Commit

--- count.rb
+++ count.rb
@@ -1,4 +1,7 @@
 puts "What is the input String ?"
 string = gets.chomp
 result = string.count("a-zA-Z")
+if result == 0
+    puts "It's necessary write something to use this program"
+end
 puts "#{string} has #{result} letters"
