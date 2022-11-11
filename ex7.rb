87bbfa0ec2ee82cd3887921a6bb763bb5c4d7b8e
masouto <matheusp.souto@yahoo.com.br>
2022-11-11 14:24:54 -0300

    Commit

--- ex7.rb
+++ ex7.rb
@@ -3,7 +3,11 @@ lenght = gets.chomp.to_i
 puts "What is the width of the room in feet ?"
 width = gets.chomp.to_i
 puts "You entered dimensions of #{lenght} feet by #{width} feet."
-CONSTANT = 0.09290304
-areaf = (lenght * width)
-aream = (areaf * CONSTANT).round(2)
-puts "The are is:\n#{areaf} square feet\n#{aream} square meters"
+CONST = 0.09290304
+def areaF(lenght,width)
+    (lenght * width)
+end
+def areaM(areaF(lenght,width),CONST)
+    (areaF(lenght,width)*CONST).round(2)
+end
+puts "The are is:\n#{areaF(lenght,width)} square feet\n#{areaM(areaF(lenght,width),CONST)} square meters"
